93c40c94f6dd3254687bcac3f67812558133d3e3
alanhemriquefeitosaribeiro <166571627+alanhemriquefeitosaribeiro@users.noreply.github.com>
2024-11-12 22:32:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(                                                                                                                                          
+    'Portugues',
+    'O que é verbo ',
+    ' verbo como a palavra que indica uma ação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Historia',
+    'quem descobril o brasil ?',
+    'pedro Álvares cabral'
+)
+
+criaCartao(
+    'Quimica ',
+    'o que são subistancias Quimicas ?',
+    'Substâncias são grupos de moléculas iguais'
+)
